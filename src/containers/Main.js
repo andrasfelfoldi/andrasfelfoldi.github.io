37e722d41f9ad263210a1c551e01095bb3464270
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-02-21 09:59:37 +0100

    initial layout

---
+++ src/containers/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Banner from './Banner';
+
+class Main extends Component {
+    
+    render() { 
+        return (
+            <div >
+                <Banner />
+            </div>
+        );
+    }
+}
+ 
+export default Main;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import React from "react"
+import "../../static/css/reset.css";
+import "../../static/css/index.css";
+import Main from "../containers/Main";
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{color: '#cfd8dc'}}><Main /></div>
