7966c804ffb6c7b3fe69475e145564ef5f225cab
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-02-21 20:47:20 +0100

    separated ScrollButton into separate component

---
+++ src/components/ScrollButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ScrollButton = () => {
+    return (
+        <div style={containerStyle}>
+            <i class="material-icons">
+                keyboard_arrow_down
+            </i>
+        </div>
+    );
+}
+ 
+export default ScrollButton;
+
+
+const containerStyle = {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'fixed',
+    bottom: '1em'
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,11 @@ import React from "react"
 import "../../static/css/reset.css";
 import "../../static/css/index.css";
 import Main from "../containers/Main";
+import ScrollButton from "../components/ScrollButton";
 
 export default () => (
     <div style={{color: '#cfd8dc'}}>
         <Main />
-        <div style={scrollButtonContainerStyle}>
-            <i class="material-icons">
-                keyboard_arrow_down
-            </i>
-        </div>
+        <ScrollButton />
     </div>
 )
-
-const scrollButtonContainerStyle = {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'fixed',
-    bottom: '1em'
-}
