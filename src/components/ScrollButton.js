095089bcdf97318a55defb9e0d0795899bb1e1db
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-02-22 09:24:08 +0100

    added initial smooth scroll

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ScrollButton = () => {
     return (
-        <div style={containerStyle}>
-            <i class="material-icons">
+        <div style={containerStyle} onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })}>
+            <i className="material-icons">
                 keyboard_arrow_down
             </i>
         </div>
@@ -19,5 +19,6 @@ const containerStyle = {
     justifyContent: 'center',
     alignItems: 'center',
     position: 'fixed',
-    bottom: '1em'
+    bottom: '1em',
+    cursor: 'pointer'
 }
