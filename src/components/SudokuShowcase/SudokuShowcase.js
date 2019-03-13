860b44d447bd88f232b609569bb8672740a62fea
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-03-13 07:31:53 +0100

    sudoku showcase now fits in all children on mobile

--- src/components/SudokuShowcase/SudokuShowcase.js
+++ src/components/SudokuShowcase/SudokuShowcase.js
@@ -32,10 +32,11 @@ class SudokuShowcase extends React.Component {
         }
 
         return (
-            <div id={this.props.id} className='container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh'}}>
+            <div id={this.props.id} className='container'>
                 <div className='flexContainer'>
                     <div className='flexItem'>
                         <FadeInMoveUp>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </FadeInMoveUp>
                     </div>
